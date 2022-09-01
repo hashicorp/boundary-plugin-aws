edc129991f7887b8aacaf99e898a3c69eeb43c36
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2022-09-01 15:25:14 -0400

    Update option handling in EC2Client and add custom HTTP client (#17)

---
+++ plugin/option.go
@@ -0,0 +1,35 @@
+package plugin
+
+// getOpts iterates the inbound Options and returns a struct
+func getOpts(opt ...Option) (options, error) {
+	opts := getDefaultOptions()
+	for _, o := range opt {
+		if o == nil {
+			continue
+		}
+		if err := o(&opts); err != nil {
+			return options{}, err
+		}
+	}
+	return opts, nil
+}
+
+// Option - how Options are passed as arguments
+type Option func(*options) error
+
+// options = how options are represented
+type options struct {
+	withRegion string
+}
+
+func getDefaultOptions() options {
+	return options{}
+}
+
+// WithRegion contains the region to use
+func WithRegion(with string) Option {
+	return func(o *options) error {
+		o.withRegion = with
+		return nil
+	}
+}
